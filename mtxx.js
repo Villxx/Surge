69680534c4018d65e822a0d9ced1420dcd01a1d3
ABXYvvv <64611305+ABXYvvv@users.noreply.github.com>
2022-04-18 13:43:38 +0800

    Update mtxx.js

--- mtxx.js
+++ mtxx.js
@@ -1,30 +1,29 @@
 /*
 QX
 [rewrite_local]
-^https:\/\/api\.xiuxiu\.meitu\.com\/v1\/user\/show\.json url script-response-body https://raw.githubusercontent.com/ABXYvvv/Surge/main/mtxx.js
+^https?://(api|h5).xiuxiu.meitu.com/(v1/user/show.json|v1/vip/vip_show.json|v1/vip/prompt/query.json|v1/h5/vip/sub_detail.json|v1/h5/user/self_show.json|v1/h5/vip/user_detail.json|v1/vip/prompt/query.json|v1/vip/prompt/query.json) url script-response-body https://raw.githubusercontent.com/ABXYvvv/Surge/main/mtxx.js
 [MITM]
-hostname: api.meiyan.com
+hostname: *.xiuxiu.meitu.com
 Surge
 [Script]
-myxj.js = requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ABXYvvv/Surge/main/mtxx.js,type=http-response,pattern=^https:\/\/api\.xiuxiu\.meitu\.com\/v1\/user\/show\.json
+myxj.js = requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ABXYvvv/Surge/main/mtxx.js,type=http-response,pattern=^https?://(api|h5).xiuxiu.meitu.com/(v1/user/show.json|v1/vip/vip_show.json|v1/vip/prompt/query.json|v1/h5/vip/sub_detail.json|v1/h5/user/self_show.json|v1/h5/vip/user_detail.json|v1/vip/prompt/query.json|v1/vip/prompt/query.json)
 [MITM]
-hostname: api.xiuxiu.meitu.com
+hostname: *.xiuxiu.meitu.com
 
-失败，只有vip图标没实际功能
-mtxx.js = requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/ABXYvvv/Surge/main/mtxx.js,type=http-response,pattern=^https:\/\/api\.xiuxiu\.meitu\.com\/v1\/user\/show\.json
-#mtxx.js = requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/huduke/Hugo/main/javaScript/unlockVip/mtxx.js,type=http-response,pattern=^https:\/\/api\.xiuxiu\.meitu\.com\/v1\/user\/show\.json
+转自iOS公社：https://iosgs.xyz/
+原脚本：https://iosgs.xyz/gzFile/quanX/mtxx.conf
 */
 
 var obj = JSON.parse($response.body);
 
 obj.data.vip_type=1;
 obj.data.expire_days=-9999999999;
-obj.data.screen_name="IOS鍏ぞ";
+obj.data.screen_name="微凉清夏";
 obj.data.is_expire=0;
 obj.data.in_valid_time=4076488923;
 obj.data.is_valid_user=1;
 obj.data.valid_time=4076488923;
-obj.data.home_prompt="鏇村娴烽噺绂忓埄锛岃鍏虫敞寰俊鍏紬鍙凤細IOS鍏ぞ";
-obj.data.home_btn_prompt="缁釜姣涚殑璐 ";
+obj.data.home_prompt="尊贵的粉钻会员";
+obj.data.home_btn_prompt="已订阅";
 
 $done({ body: JSON.stringify(obj) });
